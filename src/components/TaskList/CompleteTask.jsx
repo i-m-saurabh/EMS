80a336dee37ea5b6a39a57897bfbec710e8f69da
Saurabh <ksaurabh2005@gmail.com>
2024-12-25 12:25:17 +0530

    Complete Task list is modified with dynamic data

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -5,12 +5,10 @@ const CompleteTask = ({data}) => {
         <div className="h-full flex-shrink-0 w-[300px] p-5 bg-blue-400 rounded-xl">
             <div className='flex justify-between items-center'>
                 <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-                <h4 className='text-sm'>20 feb 2024</h4>
+                <h4 className='text-sm'>{data.taskDate}</h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-            <p className='text-sm mt-2'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta velit, veritatis id accusamus dolores excepturi.
-            </p>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+            <p className='text-sm mt-2'>{data.taskDescription}</p>
             <div className="mt-2">
                 <button className="bg-green-500 py-1 px-2 text-xs w-full">Completed</button>
             </div>
